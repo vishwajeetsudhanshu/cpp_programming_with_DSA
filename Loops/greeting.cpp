e9bc07e754666638a272d9ce8265ee120d5dc9bf
vishwajeetsudhanshu <vkshudhanshu4u@gmail.com>
2024-09-30 19:22:30 +0530

    Loops 1

---
+++ Loops/greeting.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //repetation
+    for(int i=1;i<=10;i++){
+        cout<<"Good Morning"<<endl;
+    }
+}
---
+++ Loops/ntimes_Hello.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+   int x;
+   cout<<"How many times you want to enter Hello: ";
+   cin>>x;
+   for(int i=1;i<=x;i++){
+    cout<<"Hello Guyss!"<<endl;
+   }
+}
