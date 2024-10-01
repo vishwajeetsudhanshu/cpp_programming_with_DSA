1ad24d4904dbd0888c756f32b671e4f1cc92b65e
vishwajeetsudhanshu <vkshudhanshu4u@gmail.com>
2024-10-01 19:20:11 +0530

    Loops 1

--- Loops/practice.cpp
+++ Loops/practice.cpp
@@ -1,13 +1,19 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int i;
+//int i;
     // cout<<"Enter no: ";
     // cin>>n;
     // for(i=n;i>=1;i--){
     //     cout<<i<<endl;
     // }
-    for(i=100;i>0;i=i-3){
-        cout<<i<<endl;
+    // for(i=100;i>0;i=i-3){
+    //     cout<<i<<endl;
+    // }
+    int i=1;
+    while(i==1){
+        i=i-1;
+        cout<<i<<" ";
+        
     }
 }
