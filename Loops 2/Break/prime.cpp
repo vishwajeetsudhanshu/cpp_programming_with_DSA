793c7d9f5e97dcfd40e4fd5302b7771bd409147f
vishwajeetsudhanshu <vkshudhanshu4u@gmail.com>
2024-12-11 19:48:50 +0530

    Updated

--- Loops 2/Break/prime.cpp
+++ Loops 2/Break/prime.cpp
@@ -2,6 +2,17 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int i,n;
-    cout<<"Enter a number: "
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    bool flag=true;
+    for(int i=2;i<n-1;i++){
+        if(n%i==0){
+            flag = false;
+            break;
+        }
+    }
+    if(n==1) cout<<"1 is neither a prime nor a composite";
+    else if(flag==true)cout<<n<<" is a prime";
+    else cout<<n<<" is a composite number";
 }
