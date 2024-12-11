d0a3ac3da42dc163e669be7b21e24659fd5fb5e1
vishwajeetsudhanshu <vkshudhanshu4u@gmail.com>
2024-12-11 19:26:53 +0530

    Updated

---
+++ Loops 2/Break/composite_no.cpp
@@ -0,0 +1,15 @@
+//break;
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    for(int i=2;i<=n-1;i++){
+        if(n%i==0){
+            cout<<n<<" is a composite number";
+            break;
+        }
+    }
+
+}
--- Loops 2/Break/prime.cpp
+++ Loops 2/Break/prime.cpp
@@ -1,7 +1,7 @@
-//WAP to check if a number is a prime or not
+//WAP to check if a number is a prime or not prime no means no factor other than 1 and itself
 #include<iostream>
 using namespace std;
 int main(){
     int i,n;
-    cout<<"Enter a number:"
+    cout<<"Enter a number: "
 }
