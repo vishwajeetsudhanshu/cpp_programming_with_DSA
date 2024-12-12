7052c706a7814204c3e82fa74edfb261f83e7051
vishwajeetsudhanshu <vkshudhanshu4u@gmail.com>
2024-12-12 19:46:01 +0530

    Updated

--- Loops 2/Questions_using_operators/Sum_alternate.cpp
+++ Loops 2/Questions_using_operators/Sum_alternate.cpp
@@ -1,14 +1,17 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n;
+    int i, n;
     cout<<"Enter a number : ";
     cin>>n;
     int sum=0;
-    for(int i=1;i<=n;i++){
-        if(i%2!=0) sum+=i;
-        else sum-=i;
+    // for(int i=1;i<=n;i++){
+    //     if(i%2!=0) sum+=i;
+    //     else sum-=i;
 
-    }
+    // }
+    // cout<<sum;
+    if(n%2==0) sum=-n/2;
+    else sum=-n/2+n;
     cout<<sum;
 }
