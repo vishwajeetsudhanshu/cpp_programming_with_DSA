89e49f66f8813e2db2ffbd44be967a0ef1487ba7
vishwajeetsudhanshu <vkshudhanshu4u@gmail.com>
2024-12-13 18:55:02 +0530

    Updated

---
+++ Pattern Printing/Pattern Printing 1/SOlid_rectangel.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int r,c;
+    cout<<"Enter the no. of rows: ";
+    cin>>r;
+    cout<<"Enter the no. of columns : ";
+    cin>>c;
+    if
+}
