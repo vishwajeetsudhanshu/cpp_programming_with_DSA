1807be7066988c73cc733729e4f267aaa304f8f1
vishwajeetsudhanshu <vkshudhanshu4u@gmail.com>
2025-01-16 19:26:12 +0530

    Binary Triangle

---
+++ Pattern Printing/Pattern Printing 1/Binary_tri.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the no. : ";
+    cin>>n;
+    // int k=5;
+    // for(int i=1;i<=n;i++){
+    //     if(i%2!=0) k=1;
+    //     else k=0;
+    //     for(int j=1;j<=i;j++){
+    //         cout<<k;
+    //         if(k==1) k=0;
+    //         else k=1;
+    //     }
+    //     cout<<endl;
+    // }
+    for (int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            if((i+j)%2==0) cout<<1;
+            else cout<<0;
+        }
+        cout<<endl;
+    }
+    
+}
