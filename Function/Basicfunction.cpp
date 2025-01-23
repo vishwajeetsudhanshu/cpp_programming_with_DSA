fd168f7cc9d857b830eb08d92c1c49db75ed75af
vishwajeetsudhanshu <vkshudhanshu4u@gmail.com>
2025-01-23 18:58:52 +0530

    Function

---
+++ Function/Basicfunction.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,a;
+    cout<<"Enter a no: ";
+    for(int k=1;k<=a;k++){
+        cin>>n;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                cout<<"*";
+        }
+        cout<<endl; 
+    }
+   }
+}
