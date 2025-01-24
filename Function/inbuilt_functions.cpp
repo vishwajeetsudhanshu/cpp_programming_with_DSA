8350f91a3df4f4da2288f2d271b82975ac796b54
vishwajeetsudhanshu <vkshudhanshu4u@gmail.com>
2025-01-24 10:07:24 +0530

    Inbuilt Functions

---
+++ Function/inbuilt_functions.cpp
@@ -0,0 +1,20 @@
+//min,max,sqrt
+#include<iostream>
+#include<cmath>
+using namespace std;
+// int minimum(int x,int y){
+//     int a;
+//     if(x<y) a=x;
+//     else a=y;
+//     return a;
+// }
+int main(){
+    int x,y;
+    cin>>x>>y;
+    cout<<"Minimum no. is "<<min(x,y)<<endl;
+    cout<<"Maximum no. is "<<max(x,y)<<endl;
+    cout<<"Square root of "<<x<<"is "<<sqrt(x);
+    
+    
+
+}
