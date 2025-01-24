4b20c4604867146c957199b74f39f238871623dd
vishwajeetsudhanshu <vkshudhanshu4u@gmail.com>
2025-01-24 09:19:03 +0530

    Basic

---
+++ Function/basic.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+void greeting(){
+    cout<<"Good Morning"<<endl;
+    cout<<"Have a nice day"<<endl;
+
+}
+int main(){
+    greeting();//function call
+    greeting();
+    greeting();
+}
