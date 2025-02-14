e16d9b8dcea847ec62b10d18897c01063a2dafb8
vishwajeetsudhanshu <vkshudhanshu4u@gmail.com>
2025-02-14 18:12:57 +0530

    Array

---
+++ Array/Array Part 1/Synta_decl.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[7]={0,5,10,15,20,25,30};
+    // arr[0]=0;
+    // arr[1]=5;
+    // arr[2]=10;
+    // arr[3]=15;
+    // arr[4]=20;
+    // arr[5]=25;
+    // arr[7]=30;
+    cout<<arr[5];
+}
