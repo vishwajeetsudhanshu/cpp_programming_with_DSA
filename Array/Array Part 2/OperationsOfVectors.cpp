429cab81524a1226f5016b992aa5316b611bd500
Vishwajeet Kumar Sudhanshu <124762061+vishwajeetsudhanshu@users.noreply.github.com>
2025-06-30 20:34:40 +0530

    Update OperationsOfVectors.cpp

--- Array/Array Part 2/OperationsOfVectors.cpp
+++ Array/Array Part 2/OperationsOfVectors.cpp
@@ -7,4 +7,21 @@ int main(){
     v.push_back(1);
     v.push_back(9);
     v.push_back(10);
+    v.push_back(6);
+    v.push_back(1);
+    v.push_back(9);
+    v.push_back(10);
+    v.push_back(6);
+    v.push_back(1);
+    v.push_back(9);
+    v.push_back(10);
+    for(int i=0;i<v.size();i++){
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+    v.pop_back();
+    v.pop_back();
+    for(int i=0;i<v.size();i++){
+        cout<<v[i]<<" ";
+    }
 }
