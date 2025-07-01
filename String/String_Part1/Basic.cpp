9d02975695d096946a92f3e6847cd092ceb8956b
Vishwajeet Kumar Sudhanshu <124762061+vishwajeetsudhanshu@users.noreply.github.com>
2025-07-01 22:27:09 +0530

    String Part 1

---
+++ String/String_Part1/Basic.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char str[]={'a','b','c','d','e'};//stringgggggg
+    for (int i=0;i<=4;i++){
+        cout<<str[i]<<" ";
+    }
+    //cout<<"My Name is Sanny";//this is string
+}
